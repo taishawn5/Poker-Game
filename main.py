41a6438257c29ea1da64eaf307d2f898a88b24fc
taishawn5 <72168983+taishawn5@users.noreply.github.com>
2022-05-17 11:30:42 -0700

    creating poker table

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from deck import *
 from players import *
 
-deck = Deck()
-deck.shuffle()
+# deck = Deck()
+# deck.shuffle()
 
+# bob = Player("Bob")
+# bob.draw(deck).draw(deck)
+# bob.showHand()
 bob = Player("Bob")
-bob.draw(deck).draw(deck)
-bob.showHand()
+tai = Player("Tai")
+han = Player("Han")
+soph = Player("Soph")
+luke = Player("Luke")
+kon = Player("Kon")
+
+table = Table()
+table.create_table([bob,tai,han,soph,luke,kon])
+table.show_table()
+
+
+
 
