1364290fcb9c7192045f7817facb471c6160b0b4
taishawn5 <72168983+taishawn5@users.noreply.github.com>
2022-05-17 11:29:50 -0700

    Creating poker table

--- players.py
+++ players.py
@@ -1,3 +1,4 @@
+from multiprocessing.sharedctypes import Value
 from deck import *
 
 class Player():
@@ -14,9 +15,30 @@ class Player():
     def discard(self):
         return self.hand.pop()
 
-class Table():
-    def __init__(self, amt_players):
-        self.amt_players = amt_players
-    
-    def create_table():
-        pass
+class Node:
+    def __init__(self,player):
+        self.player = player
+        self.next = None
+
+class Table:
+    def __init__(self):
+        self.tail = None
+        self.amt_players = 6
+
+    def create_table(players):
+        for i in range(self.amt_players):
+            if self.list_is_empty():
+                new_node = Node(players[i])
+                self.tail = new_node
+            else:
+                new_node = Node(players[i])
+                new_node.next = self.tail.next
+                self.tail.next = new_node
+
+    def show_table():
+        if self.tail == None:
+            print("The table is empty")
+        temp = self.tail.next
+        while temp is not self.tail:
+            print(temp.player.name)
+            temp = temp.next
